86a008fd29490efe0ff69139d08b4a84ae473714
Ilham Azhari <azhariilham@gmail.com>
2020-02-16 18:52:30 +0700

    Change library using https

--- resources/views/scanner.blade.php
+++ resources/views/scanner.blade.php
@@ -17,8 +17,8 @@
 
 @section('script')
 <script type="module">
-  import QrScanner from "{{asset('/js/qr-scanner.min.js')}}";
-  QrScanner.WORKER_PATH = "{{asset('/js/qr-scanner-worker.min.js')}}";
+  import QrScanner from "https://bookingevent.herokuapp.com//js/qr-scanner.min.js";
+  QrScanner.WORKER_PATH = "https://bookingevent.herokuapp.com//js/qr-scanner-worker.min.js";
 
   const camera = document.getElementById('camera');
   const qrResult = document.getElementById('qrResult');
